49df57aa929db2d402e9f9de578bf3a4112ee8db
cagindex <2429307828@qq.com>
2023-02-26 15:51:26 +0800

    day1

---
+++ HW/HW1/student_info.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <cstring>
+#include <sstream>
+#include <cstdlib>
+using namespace std;
+
+class Student {
+// 在此处补充你的代码
+};
+
+int main() {
+	Student student;        // 定义类的对象
+	student.input();        // 输入数据
+	student.calculate();    // 计算平均成绩
+	student.output();       // 输出数据
+}
