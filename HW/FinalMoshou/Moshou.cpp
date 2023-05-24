0669e48986d49db6352f14f66e6691461141b91c
cagindex <2429307828@qq.com>
2023-05-24 16:26:46 +0800

    Modify

--- HW/FinalMoshou/Moshou.cpp
+++ HW/FinalMoshou/Moshou.cpp
@@ -1,12 +1,16 @@
 /*****************************
+ 变量用下划线，class用非首字母大写，函数方法用大写。
 完成对象的构造函数、对应属性以及基本变量。
 *****************************/
 #include<iostream>
 #include<cstring>
+#include<map>
+#include<iomanip>
 #define INTMAX 0x7fffffff
 using std::cout;
 using std::endl;
 using std::string;
+class gameMap;
 class warrior;
 class weapon;
 class baseCity;
@@ -24,14 +28,27 @@ class bomb;
 /**
     basic variable
 */
-int M, N, R, K, T;
+int warrior_kind = 5; // only 5 kinds of warriors
+int M, N, R, K, T;// N -> city num; R arrow atk; k loyalty decrease; T game max time
 int battle_bonus = 8;
 int city_health_round = 10;
+gameMap* GAME;
+std::map<int, string> red_headquarter_warrior = 
+{
+    {0, "iceman"}, {1, "lion"}, {2, "wolf"}, {3, "ninja"}, {4, "dragon"}
+ };
+std::map<int, string> blue_headquarter_warrior =
+{
+    {0, "lion"}, {1, "dragon"}, {2, "ninja"}, {3, "iceman"}, {4, "wolf"}
+};
 
 /**
  * basic function declartion
 */
+int TellMeTheTime(gameMap* game);
+void BornReport(char flag, int id);
 void CreateWeapon(warrior*, weapon*& , int);
+
 
 /**
  * base class start
@@ -39,7 +56,7 @@ void CreateWeapon(warrior*, weapon*& , int);
 //base class for dragon ninja iceman lion wolf
 class warrior
 {
-    private:
+    protected:
         int id, health, atk;
         char flag;
     public:
@@ -52,11 +69,10 @@ class baseCity
 {
     private:
         int position;
-        char flag;
         warrior* warrior_red = nullptr;
         warrior* warrior_blue = nullptr;
     public:
-        baseCity(char _f, int _pos = 0):position(_pos),flag(_f){}
+        baseCity(int _pos = 0):position(_pos){}
 };
 
 //base class for sword arrow and bomb
@@ -85,6 +101,7 @@ class dragon : public warrior
         dragon(char _f, int _id = 0, int _health = 0, int _atk = 0, double _morale = 0):warrior(_f, _id, _health, _atk),morale(_morale)
         {
             CreateWeapon(this, weapon_dragon, _id%3);
+            BornReport(_f, _id);
         }
 };
 
@@ -97,6 +114,7 @@ class ninja : public warrior
         {
             CreateWeapon(this, weapon_ninja1, _id%3);
             CreateWeapon(this, weapon_ninja1, (_id+1)%3);
+            BornReport(_f, _id);
         }
 };
 
@@ -108,6 +126,7 @@ class iceman : public warrior
         iceman(char _f, int _id = 0, int _health = 0, int _atk = 0):warrior(_f, _id, _health, _atk)
         {
             CreateWeapon(this, weapon_iceman, _id%3);
+            BornReport(_f, _id);
         }
 };
 
@@ -116,7 +135,10 @@ class lion : public warrior
     private:
         int loyalty;
     public:
-        lion(char _f, int _id = 0, int _health = 0, int _atk = 0, int _loyalty = 0):warrior(_f, _id, _health, _atk),loyalty(_loyalty){}
+        lion(char _f, int _id = 0, int _health = 0, int _atk = 0, int _loyalty = 0):warrior(_f, _id, _health, _atk),loyalty(_loyalty)
+        {
+            BornReport(_f, _id);
+        }
 };
 
 class wolf : public warrior
@@ -128,6 +150,7 @@ class wolf : public warrior
         {
             weapon_wolf = new weapon*[3];
             for(int i = 0; i < 3; ++i) weapon_wolf[i] = nullptr;
+            BornReport(_f, _id);
         }
 };
 
@@ -135,19 +158,21 @@ class wolf : public warrior
 class city : public baseCity
 {
     private:
+        int cityFlag = 0;
         int city_health = 0;
     public:
-        city(char _f, int _pos = 0):baseCity(_f, _pos){}
+        city(int _pos = 0):baseCity(_pos){}
 };
 
 // headquarter class for both side
 class headquarter : public baseCity
 {
     private:
+        char flag;        
         int health = M;
         warrior* Other_warrior = nullptr;
     public:
-        headquarter(char _f, int _pos = 0):baseCity(_f, _pos){}
+        headquarter(char _f, int _pos = 0):baseCity(_pos),flag(_f){}
 };
 
 // weapon class
@@ -175,8 +200,55 @@ class bomb : public weapon
 */
 
 /**
- * basic function implement
+ * game map class
 */
+class gameMap
+{
+    private:
+        int game_time = 0;
+        baseCity** citys;
+    public:
+        gameMap(int n)
+        {
+            citys = new baseCity*[n+2];
+            for(int i = 0; i <= n+1; ++i)
+            {
+                if(i == 0) citys[i] = new headquarter('R', i);
+                else if(i == n+1) citys[i] = new headquarter('B', i);
+                else citys[i] = new city(i);
+            }
+        }
+
+        // generate warriors
+        void GenerateWarrior()
+        {
+
+        }
+
+        friend int TellMeTheTime(gameMap* game);
+};
+
+/**
+ * basic function implement start
+*/
+int TellMeTheTime(gameMap* game)
+{
+    return game->game_time;
+}
+
+void BornReport(char flag, int id)
+{
+    int time = TellMeTheTime(GAME);
+    if(flag == 'R') 
+    cout << std::setw(3) << std::setfill('0')
+    << (time / 60) << ":" << std::setw(2) << std::setfill('0') << (time % 60) << " red " << 
+    red_headquarter_warrior[id%5] << " " << id << " " << "born" << endl;
+    if(flag == 'B') 
+    cout << std::setw(3) << std::setfill('0')
+    << (time / 60) << ":" << std::setw(2) << std::setfill('0') << (time % 60) << " blue " << 
+    blue_headquarter_warrior[id%5] << " " << id << " " << "born" << endl;
+}
+
 void CreateWeapon(warrior* warrior_pointer, weapon*& weapon_pointer, int id)
 {
     switch(id)
@@ -186,6 +258,9 @@ void CreateWeapon(warrior* warrior_pointer, weapon*& weapon_pointer, int id)
         case 2 : weapon_pointer = new bomb(); break;
     }
 }
+/**
+ * basic function implement end
+*/
 
 
 int main(){
--- HW/FinalMoshou/test.cpp
+++ HW/FinalMoshou/test.cpp
@@ -1,43 +1,13 @@
 #include<iostream>
 #include<cstring>
+#include<map>
+#include<iomanip>
 using namespace std;
-class character;
 
-class A{
-    private:
-        friend class character;
-        int durity;
-    public:
-        A(int a):durity(a){}
-        ~A(){cout << "A deleted" << endl;}
-        void use(){durity--;}
-};
-
-class character{
-    public:
-        A** container;
-        character()
-        {
-            container = new A*[3];
-            for(int i = 0; i < 3; ++i)
-            {
-                container[i] = new A(i+1);
-            }
-        }
-        void use(int index)
-        {
-            container[index]->use();
-            if(container[index]->durity == 0)
-            {
-                delete container[index];
-                container[index] = nullptr;
-            } 
-        }
-};
 
 int main(){
-    character windy;
-    windy.use(1);
-    windy.use(1);
+    double a = 3.146;
+    cout << fixed << setprecision(2) << a << endl;
+    printf("%.2f\n", a);
     return 0;
 }
